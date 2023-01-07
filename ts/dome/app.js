16db89352ead81a8664d9225c6bcd4e5f5d068ce
sunboy <38577739+hyxieshi@users.noreply.github.com>
2023-01-07 14:06:00 +0800

    =-=

---
+++ ts/dome/app.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: SunBOY
+ * @Date: 2023-01-03 16:24:40
+ * @LastEditors: SunBOY
+ * @LastEditTime: 2023-01-03 16:28:49
+ * @FilePath: \web-dotolist\ts\dome\app.ts
+ * @Description:
+ * Copyright 2023 OBKoro1, All Rights Reserved.
+ * 2023-01-03 16:24:40
+ */
+var b;
+b = 12312;
+console.log(b);
